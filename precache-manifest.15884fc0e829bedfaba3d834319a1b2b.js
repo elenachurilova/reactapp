2a2b704e494855bb78886e8c7e8daaeea68222d7
Elena Churilova <ellena.churilova@gmail.com>
2020-10-24 15:13:50 -0700

    Updates

---
+++ precache-manifest.15884fc0e829bedfaba3d834319a1b2b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff423f47ec70d6167ef65e50563a3282",
+    "url": "/reactapp/index.html"
+  },
+  {
+    "revision": "0a7a16482fc546e3ef26",
+    "url": "/reactapp/static/css/main.38554735.chunk.css"
+  },
+  {
+    "revision": "953e61e4f28ea32ac1c3",
+    "url": "/reactapp/static/js/2.7bf9e3bb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactapp/static/js/2.7bf9e3bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a7a16482fc546e3ef26",
+    "url": "/reactapp/static/js/main.65f8e04f.chunk.js"
+  },
+  {
+    "revision": "e219a2030a13ac34f0be",
+    "url": "/reactapp/static/js/runtime-main.f935dd26.js"
+  }
+]);
